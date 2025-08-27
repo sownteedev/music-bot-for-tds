b58a3327bcdad39180d92843b457400cb2623290
sownteedev <sownteenguyen@gmail.com>
2025-08-27 12:45:13 +0700

    :>

--- bot-distube.js
+++ bot-distube.js
@@ -121,18 +121,7 @@ const distube = new DisTube(client, {
         new YouTubePlugin({
             ytdlOptions: {
                 quality: 'highestaudio',
-                filter: 'audioonly',
-                requestOptions: {
-                    headers: {
-                        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
-                        'Accept-Language': 'en-US,en;q=0.9',
-                        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-                        'Accept-Encoding': 'gzip, deflate',
-                        'DNT': '1',
-                        'Connection': 'keep-alive',
-                        'Upgrade-Insecure-Requests': '1'
-                    }
-                }
+                filter: 'audioonly'
             }
         }),
         new SpotifyPlugin()
